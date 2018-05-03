bed32c97d14042c7553c527ef3b7f61f3913b2e4
bryan317 <bryan.gtz.317@gmail.com>
2018-05-03 05:01:13 +0300

    .
    .

---
+++ subir.php
@@ -0,0 +1,74 @@
+<?php 
+
+$page_title = "Subir anuncio";
+include 'includes/menu.php'; 
+if ($_SERVER['REQUEST_METHOD']=='POST') {
+
+	// Conectar a la base de datos
+	require ("mysqli_connect_is.php");
+
+	// Arreglo para almacenar mensajes de error
+	$error = array();
+
+	// Verificar que se proporcione el Título
+	if (isset($_POST['titulo'])&&empty($_POST['titulo']))
+		$error[]="Olvidó introducir el Título.";
+	else{
+		$titulo = mysqli_real_escape_string($conexion,$_POST['titulo']);
+		$titulo = trim($titulo);
+	}
+	if (!empty($_FILES['archivo'])){
+			if (is_uploaded_file($_FILES['archivo']['tmp_name'])){
+				// Datos del fichero
+				$ruta = $_FILES['archivo']['tmp_name'];	// dirección temporal
+				$name = $_FILES['archivo']['name'];	// nombre del archivo
+				$tama = $_FILES['archivo']['size']; //tamaño del archivo
+				$nruta = 'files/archivo/'.$name;	//nueva direccion
+				$array = explode('.', $name); //split con .
+				$ext = end($array);	// Obtenemos la extensión el archivo
+				$pdf = 'pdf';
+
+				if ($ext == $pdf){
+					$archivo = $nruta;
+				}
+				else{
+					$error[] = 'Asegúrese de subir un archivo tipo PDF';
+				}
+			}
+			else{
+				$error[] = 'Olvidó introducir el Archivo';
+			}
+	}
+}
+
+?>
+		<form enctype="multipart/form-data" id="form" method="post"	action="subir.php">
+			<fieldset>
+				<label for="titulo" class="txt">Título:</label>
+				<input type="text" class="pw lab" id="titulo" name="titulo" required="required" value="<?php if(isset($_POST['titulo'])) echo $_POST['titulo']; ?>"><br><br>
+				<div class="centrado">
+					<input type="file" name="archivo" class="inputFile centrado" required="required">
+				</div>
+				<div class="centrado">
+					<label><em>Solo archivos en formato pdf. Máximo: 4MB</em></label>
+				</div>
+				<div class="centrado">
+					<span class="error">
+						<?php
+							if (!empty($error)){
+								foreach ($error as $msg) {	// Mostrar cada error
+								echo "<b class='error'>*$msg</b> <br>\n";
+								}
+							}
+						?>
+					</span>
+				</div>
+			<div class=centrado>
+				<button class="boton">Guardar</button>
+				<button class="boton" type="reset">Limpiar</button>
+			</div>
+			</fieldset>
+		</form>
+	</div>
+</body>
+</html>
