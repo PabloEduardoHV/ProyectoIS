33a44431044065222a113f41c75deb537100d8c1
ghostcea <38966651+ghostcea@users.noreply.github.com>
2018-05-04 01:42:01 -0500

    Update menu.php

--- includes/menu.php
+++ includes/menu.php
@@ -2,24 +2,21 @@
 <html>
 <head>
 	<title><?php echo $page_title; ?></title>
-	<link rel="stylesheet" type="text/css" href="comun/css/estilo.css">
+
+		<link href="comun/css/estilo.css" rel="stylesheet" type="text/css">
+		<link href="comun/css/menu.css" rel="stylesheet" type="text/css">
 	<script type="text/javascript" src="comun/js/jquery.js"></script>
 	<script type="text/javascript" src="comun/js/ventana.js"></script>
 </head>
 <body>
-	<aside class="aside">
-	</aside>
-	<div class="float">
-		<figure>
-			<a href="index.php">
-				<img alt="Cerrar sesión" src="comun/imagenes/logout.png" width="100%">
-			</a>
-		</figure>
-		<aside class="aside2">
-		</aside>
-	</div>
-	<div class="container fondo">
-		<center><h1 class="ledpard">LEDPARD</h1></center>
+	<div class="container">
+			
+	      <div class="sidebar1">
+
+	      </div>
+
+		  <div class="content">
+		<h1 class="ledpard">LEDPARD</h1>
 		<header>
 			<nav>
 				<ul>
@@ -34,3 +31,4 @@
 				</ul>
 			</nav>
 		</header>
+
