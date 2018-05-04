8eec9382692de01f6d8e3ca2039b63c5666aeb75
BryanGtz <38710459+BryanGtz@users.noreply.github.com>
2018-05-04 02:47:44 -0500

    Update configuracion.php

--- configuracion.php
+++ configuracion.php
@@ -1,13 +1,65 @@
 <?php
 // Verificar si se inició sesión
-/*session_start();
-if($_SESSION['user'] != 'coordinador'){
+session_start();
+if($_SESSION['tipo'] != 'admin1'){
 	header("Location:index.php");
-}*/
+}
 
 $page_title = "Configuracion"; 	// Nombre de la pestaña
 include 'includes/menu.php'; // incluir en diseño del menú a la página
-?>
+
+if ($_SERVER['REQUEST_METHOD']=='POST') {
+	require '../mysqli_connect_is.php';
+	$error = array();
+	if(empty($_POST['oldPass'])){
+		$error[] = "Olvidó ingresar la contraseña actual";
+	}
+	else{
+		$oldPass = $_POST['oldPass'];
+	}
+	if(empty($_POST['newPass'])){
+		$error[] = "Olvidó ingresar la contraseña nueva";
+	}
+	else{
+		$newPass = $_POST['newPass'];
+	}
+
+	$query = "call IdentificarUsr('$oldPass')";
+	$resul = mysqli_query($conexion,$query);
+	if($resul){
+		$num = mysqli_num_rows($resul);
+        if($num>0){
+            while($fila = mysqli_fetch_array($resul)){
+                $cuenta=$fila[0];
+            }
+            mysqli_next_result($conexion);
+            mysqli_free_result($resul);
+            if($cuenta==$_SESSION['tipo']){
+            	$query = "call CambContraseña('".$_SESSION['tipo']."', '$newPass')";
+            	$resul = mysqli_query($conexion,$query);
+            	if($resul){
+            		mysqli_next_result($conexion);
+            		mysqli_free_result($resul);
+            		echo '<script>alert("La contraseña fue cambiada exitosamente")</script>';
+                	echo "<script>location.href='configuracion.php'</script>";
+            	}
+            	else{
+            		$error[] = "No se pudo cambiar su contraseña. Intente mas tarde.";
+            		echo '<p>'.mysqli_error($conexion).'<br>Query: '.$query.'</p>';
+            	}
+            }
+            else{
+				$error[] = "Contraseña actual equivocada";
+			}
+        }
+
+	}
+	else{
+		$error[] = "No se pudo validar su contraseña actual. Intente mas tarde.";
+		echo '<p>'.mysqli_error($conexion).'<br>Query: '.$query.'</p>';
+	}
+
+}
 
 <form id="form" method="post" action="configuracion.php">
 			<fieldset class="centrado">
