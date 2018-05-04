1cf4c9414b3e2729664a7ca40463b1035cff658b
ghostcea <38966651+ghostcea@users.noreply.github.com>
2018-05-04 01:19:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,26 +3,30 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<title>NOTIFITEC</title>
-	<link rel="stylesheet" type="text/css" href="comun/css/estilo.css">
-</head>
-<body>
-	<aside class="aside">
-	</aside>
-	<div class="float">
-		<figure>
-			<a href="login.php">
-				<img src="comun/imagenes/login.png" width="100%">
-			</a>
-		</figure>
-		<aside class="aside2">
-		</aside>
-	</div>
-	<div class="container fondo">
-		<?php ?>
-	</div>
+	<head>
+		<meta charset="utf-8">
+		<title>NOTIFITEC</title>
+		<link href="comun/css/estilo.css" rel="stylesheet" type="text/css">
+		<link href="comun/css/menu.css" rel="stylesheet" type="text/css">
+	</head>
 
-	
-</body>
+	<body>
+		<div class="container">
+			
+	      <div class="sidebar1">
+
+	      </div>
+
+		  <div class="content">
+			<p>Aqui van los anuncios</p>
+			<h2>No hay anuncios en este momento</h2>
+		  </div>
+
+	      <div class="sidebar2">
+	      	<a href="login.php">
+				<img src="comun/imagenes/login.png" width="80%">
+			</a>
+	      </div>
+  		</div>
+	</body>
 </html>
